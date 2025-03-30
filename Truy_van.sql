ad7e861c3869939e9779547b56c8ccc65a8e1298
Truongquyen2004 <163295071+Truongquyen2004@users.noreply.github.com>
2025-03-30 16:25:46 +0700

    Add files via upload

---
+++ Truy_van.sql
@@ -0,0 +1,17 @@
+SELECT 
+    sv.masv, 
+    sv.hoten, 
+    mh.mamon, 
+    mh.Tenmon, 
+    dkmh.maLopHP, 
+    lhp.TenLopHP, 
+    dkmh.DiemThi, 
+    dkmh.PhanTramThi, 
+    SUM(d.diem * (1 - dkmh.PhanTramThi / 100)) AS DiemThanhPhan
+FROM DKMH dkmh
+JOIN Diem d ON dkmh.id_dk = d.id_dk
+JOIN SinhVien sv ON dkmh.maSV = sv.masv
+JOIN LopHP lhp ON dkmh.maLopHP = lhp.maLopHP
+JOIN MonHoc mh ON lhp.maMon = mh.mamon
+GROUP BY sv.masv, sv.hoten, mh.mamon, mh.Tenmon, dkmh.maLopHP, lhp.TenLopHP, dkmh.DiemThi, dkmh.PhanTramThi;
+
